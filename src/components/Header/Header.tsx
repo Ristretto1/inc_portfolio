ca3b176c9f561f2c5bf3f9e80dc0ff102165e313
Antonov Kirill <antonovkir2@gmail.com>
2022-05-30 09:39:05 +0500

    add Header comp

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React, {FC} from 'react';
+import s from './Header.module.css';
+
+type HeaderPropsType = {}
+
+export const Header: FC<HeaderPropsType> = ({}) => {
+    return (
+        <div className={s.header}>
+
+        </div>
+    );
+};
+
