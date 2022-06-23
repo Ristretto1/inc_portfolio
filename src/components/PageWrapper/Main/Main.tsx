6fb07737627a20842d121f0faa9f0277c63cebf2
Antonov Kirill <antonovkir2@gmail.com>
2022-06-23 20:34:16 +0500

    new text

--- src/components/PageWrapper/Main/Main.tsx
+++ src/components/PageWrapper/Main/Main.tsx
@@ -7,7 +7,7 @@ export const Main = () => {
         <section className={s.main}>
             <div className={s.text}>
                 <h2>Приветствую</h2>
-                <h1>I'm a Creative Developer & Technology Expert</h1>
+                <h1>Я Frontend-developer</h1>
             </div>
         </section>
     );
