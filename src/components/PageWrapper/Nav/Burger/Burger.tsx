b54d2a3e7debaa1ab74fa1d6e914b2f8c9bbb315
Antonov Kirill <antonovkir2@gmail.com>
2022-06-29 21:03:02 +0500

    add burger comp

---
+++ src/components/PageWrapper/Nav/Burger/Burger.tsx
@@ -0,0 +1,32 @@
+import React, {FC, useState} from 'react';
+import s from './Burger.module.css';
+
+
+type BurgerPropsType = {
+    callback: (show: boolean) => void
+}
+
+export const Burger: FC<BurgerPropsType> = ({callback}) => {
+
+    const [show, setShow] = useState<boolean>(false)
+
+    const onBurgerHandler = () => {
+        setShow(state => !state)
+        callback(!show)
+    }
+
+    return (
+        <div
+            className={`${s.navIcon} ${show ? s.open: ''}`}
+            onClick={onBurgerHandler}
+        >
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    );
+};
+
