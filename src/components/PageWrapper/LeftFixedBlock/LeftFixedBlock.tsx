6a8693b76ea8e35b1235d1969373f99712acfcef
Antonov Kirill <antonovkir2@gmail.com>
2022-06-30 20:14:49 +0500

    remove social block

--- src/components/PageWrapper/LeftFixedBlock/LeftFixedBlock.tsx
+++ src/components/PageWrapper/LeftFixedBlock/LeftFixedBlock.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import s from './LeftFixedBlock.module.css';
-import telegramIcon from './icons/telegram.svg';
-import githubIcon from './icons/github.svg';
-import phoneIcon from './icons/phone.svg';
 
 
 export const LeftFixedBlock = () => {
@@ -10,11 +7,6 @@ export const LeftFixedBlock = () => {
         <div className={s.imgContainer}>
             <div className={s.imgWrapper}>
                 <span>Антонов Кирилл</span>
-                <ul className={s.socialBlock}>
-                    <li><a href="#"><img src={telegramIcon} alt="telegram"/></a></li>
-                    <li><a href="#"><img src={githubIcon} alt="github"/></a></li>
-                    <li><a href="#"><img src={phoneIcon} alt="phone"/></a></li>
-                </ul>
             </div>
         </div>
     );
